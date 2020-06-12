513f568cbdf353af80c45e525c708b2c5b0189c7
abeersalahuddin <52610302+abeersalahuddin@users.noreply.github.com>
2020-06-12 23:14:26 +0500

    added app

---
+++ src/skill.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Skill(props) {
+    return (
+        <div>
+            <h1>PIAIC Bootcamp2020 Skills required {props.name}</h1>
+            {/* <h1>PIAIC Bootcamp2020 Skills required {props.name}</h1>
+            <h1>PIAIC Bootcamp2020 Skills required {props.name}</h1> */}
+
+        </div>
+    );
+}
+
+export default Skill;
